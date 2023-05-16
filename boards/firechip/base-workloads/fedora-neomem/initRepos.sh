a9a26a3d27bec63520b8e2e78a4544d243211821
PKUZHOU <751722308@qq.com>
2023-05-16 14:10:35 +0800

    refine fedora neomem config

--- boards/firechip/base-workloads/fedora-neomem/initRepos.sh
+++ boards/firechip/base-workloads/fedora-neomem/initRepos.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 dnf makecache
 dnf install numactl -y
+dnf install tmux -y 
+# dnf install memcached --allowerasing
+# dnf install autoconf automake make -y
+# dnf install install pcre-devel zlib-devel libmemcached-devel libevent-devel openssl-devel -y
 poweroff
